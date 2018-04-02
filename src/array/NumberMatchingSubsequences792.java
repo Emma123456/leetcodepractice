d0d366f12a1f21e0035f25da7103cc33ac0889ec
Emma123456 <cxy510110@163.com>
2018-04-02 15:53:33 +0800

    game

--- src/array/NumberMatchingSubsequences792.java
+++ src/array/NumberMatchingSubsequences792.java
@@ -1,5 +1,6 @@
 package array;
 
+import java.util.Arrays;
 import java.util.Deque;
 import java.util.HashMap;
 import java.util.LinkedList;
@@ -37,8 +38,44 @@ public class NumberMatchingSubsequences792 {
 		return count;
 	}
 
+	/**
+	 * 第二种思路是：s的长度为n，计算每个位置i之后 [a-z]首次出现的位置。这样，当检查每一个word的时候，只要查看位置就可以
+	 * @param s
+	 * @param words
+	 * @return
+	 */
+	public int numMatchingSubseqV2(String s, String[] words) {
+		int n = s.length();
+		int[][] dp = new int[n + 1][26];
+		Arrays.fill(dp[n], -1);
+		char[] chars = s.toCharArray();
+		for (int i = n - 1; i >= 0; i--) {
+			for(int j=0;j<26;j++){
+				dp[i][j] = dp[i+1][j];
+			}
+			dp[i][chars[i]-'a'] = i+1;//???
+		}
+		int count = 0;
+		for(String word : words){
+			char[] chs = word.toCharArray();
+			int j = 0;
+			for(char ch : chs){
+				j = dp[j][ch-'a'];
+				if(j==-1){
+					break;
+				}
+			}
+			if(j!=-1){
+				count++;
+			}
+		}
+		return count;
+	}
 	public static void main(String[] args) {
-
+		String s = "abcde";
+		String[] words = new String[]{"a", "bb", "acd", "ace"};
+		int r  = new NumberMatchingSubsequences792().numMatchingSubseqV2(s, words);
+		System.out.println(r);
 	}
 
 }
