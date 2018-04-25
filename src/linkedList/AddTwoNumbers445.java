39e251a340834c18f37d6247d826a114070a1adc
Emma123456 <cxy510110@163.com>
2018-04-25 21:23:47 +0800

    add two numbers

--- src/linkedList/AddTwoNumbers445.java
+++ src/linkedList/AddTwoNumbers445.java
@@ -2,9 +2,9 @@ package linkedList;
 
 public class AddTwoNumbers445 {
 	public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
-        ListNode p = reverseList(l1);
-        ListNode q = reverseList(l2);
-        ListNode dummy = new ListNode(-1);
+		ListNode p = reverseList(l1);
+		ListNode q = reverseList(l2);
+		ListNode dummy = new ListNode(-1);
 		ListNode currNode = dummy;
 		int carry = 0;
 		while (p != null || q != null) {
@@ -12,26 +12,24 @@ public class AddTwoNumbers445 {
 			carry = sum / 10;
 			currNode.next = new ListNode(sum % 10);
 			currNode = currNode.next;
-			if (p != null)
-				p = p.next;
-			if (q != null)
-				q = q.next;
+			p = p == null ? null : p.next;
+			q = q == null ? null : q.next;
 		}
 		if (carry > 0) {
 			currNode.next = new ListNode(carry);
 		}
-        return reverseList(dummy.next);
-    }
-    
-    public ListNode reverseList(ListNode head) {
-		ListNode pre = null;
-		ListNode current = head;
-		while (current != null) {
-			ListNode nextTmp = current.next;
-			current.next = pre;
-			pre = current;
-			current = nextTmp;
-		}
-		return pre;
+		return reverseList(dummy.next);
 	}
+
+	public ListNode reverseList(ListNode head) {
+		ListNode newHead = null;
+		while (head != null) {
+			ListNode next = head.next;
+			head.next = newHead;
+			newHead = head;
+			head = next;
+		}
+		return newHead;
+	}
+
 }
--- src/linkedList/ReverseLinkedList206.java
+++ src/linkedList/ReverseLinkedList206.java
@@ -12,4 +12,15 @@ public class ReverseLinkedList206 {
 		}
 		return pre;
 	}
+	
+	public ListNode reverseListV2(ListNode head) {
+		ListNode newHead = null;
+		while (head != null) {
+			ListNode next = head.next;
+			head.next = newHead;
+			newHead = head;
+			head = next;
+		}
+		return newHead;
+	}
 }
