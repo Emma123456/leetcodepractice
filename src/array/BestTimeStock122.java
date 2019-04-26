ce787a13ef1da5bbf35706226692901ddcf8e445
cuixianyun <cxy510110@163.com>
2019-04-26 22:36:27 +0800

    贪心：找局部最小值和局部最大值

--- src/array/BestTimeStock122.java
+++ src/array/BestTimeStock122.java
@@ -18,6 +18,26 @@ public class BestTimeStock122 {
 		}
 		return maxP;
     }
+
+	/**
+	 * 这个版本比较好理解：找局部最小值和局部最大值
+	 * https://leetcode.com/problems/best-time-to-buy-and-sell-stock-ii/discuss/208241/Explanation-for-the-dummy-like-me
+	 * @param prices
+	 * @return
+	 */
+	public int maxProfitV2(int[] prices) {
+		int maxProfit = 0;
+		int i=0;
+		int n = prices.length;
+		while(i<n-1){
+			while(i<n-1 && prices[i+1]<=prices[i]) i++;
+			int buy = prices[i];
+			while(i<n-1 && prices[i+1]>prices[i]) i++;
+			int sell = prices[i];
+			maxProfit += sell - buy;
+		}
+		return maxProfit;
+	}
 	public static void main(String[] args) {
 		
 	}
