6e35b1bf0504577253bd1fcea6f022b4543e25d6
cuixianyun <cxy510110@163.com>
2020-04-10 00:19:54 +0800

    A星算法

--- src/homework2/graph/GraphV2.java
+++ src/homework2/graph/GraphV2.java
@@ -2,23 +2,35 @@ package homework2.graph;
 
 import java.util.*;
 
-public class Graph {
+public class GraphV2 {
     //顶点数量
     private int v;
     //链接表
     private LinkedList<Edge> adjacency[];
-
+    //顶点
+    private Vertext[] vertexts;
 
     /**
      * 构造顶点数为v的图
      * @param v
      */
-    public Graph(int v) {
+    public GraphV2(int v) {
         this.v = v;
         adjacency = new LinkedList[v];
         for(int i=0;i<v;i++){
             adjacency[i] =  new LinkedList<>();
         }
+        vertexts = new Vertext[v];
+    }
+
+    /**
+     * 添加顶点
+     * @param id
+     * @param x
+     * @param y
+     */
+    public void addVertex(int id, int x, int y){
+        vertexts[id] = new Vertext(id,x,y);
     }
 
     /**
@@ -36,39 +48,42 @@ public class Graph {
     }
 
     /**
-     * 计算从sid到tid的最短路径。返回值是经过路径的节点id
+     * 计算从sid到tid的最短路径。返回值是经过路径的节点id。A星算法。
      * @param sid
      * @param tid
      * @return
      */
-    public List<Integer> dijkstrala(int sid ,int tid){
-        Vertext[] vertexts = new Vertext[v];
-        for(int i=0; i < v;i++){
-            vertexts[i] = new Vertext(i);
-        }
+    public List<Integer> astar(int sid ,int tid){
         vertexts[sid].dist = 0;
-
+        vertexts[sid].f = 0;
         //predecessor[i]=得到i的前驱节点
         int[] predecessor = new int[v];
         predecessor[sid] = -1;
         PriorityQueue<Vertext> queue = new PriorityQueue<>(this.v,
                 new Comparator<Vertext>(){
                     public int compare(Vertext o1, Vertext o2){
-                        return o1.dist - o2.dist;
+                        //变化2
+                        return o1.f - o2.f;
                     }
                 });
         //队列中可能存在重复节点,会存在多余操作
         queue.offer(vertexts[sid]);
         while(! queue.isEmpty()){
             Vertext node = queue.poll();
-            if(node.id == tid) break;
             if(adjacency[node.id]!=null){
                 for(Edge edge : adjacency[node.id]){
                     int to = edge.tid;
                     if(vertexts[node.id].dist + edge.weight < vertexts[to].dist){
                         vertexts[to].dist = vertexts[node.id].dist + edge.weight;
+                        //变化3
+                        vertexts[to].f = vertexts[to].dist + hManhattan(node.id, to);
                         queue.add(vertexts[to]);
                         predecessor[to] = node.id;
+                    }
+                    //变化1
+                    if(to == tid){
+                        queue.clear();
+                        break;
                     }
                 }
             }
@@ -81,7 +96,7 @@ public class Graph {
         return path;
     }
 
-
+    
     private void visitPredecessor(int[] predecessor, int tid,List<Integer> path) {
         path.add(tid);
         if(predecessor[tid] != -1){
@@ -89,14 +104,31 @@ public class Graph {
         }
     }
 
+    /**
+     * 计算两个顶点的曼哈顿距离
+     * @param id1
+     * @param id2
+     * @return
+     */
+    private int hManhattan(int id1,int id2){
+        return Math.abs(vertexts[id1].x - vertexts[id2].x) + Math.abs(vertexts[id2].y - vertexts[id2].y);
+    }
+
     private class Vertext{
         //编号
         private int id;
         //距离
         private int dist = Integer.MAX_VALUE;
-
-        public Vertext(int id) {
+        //顶点在地图中的横坐标
+        private int x;
+        //顶点在地图中的纵坐标
+        private int y;
+        //f = g(i) + h(i)
+        private int f;
+        public Vertext(int id,int x,int y) {
             this.id = id;
+            this.x = x;
+            this.y = y;
         }
     }
     private class Edge{
---
+++ src/homework2/graph/GraphV2Test.java
@@ -0,0 +1,50 @@
+package homework2.graph;
+
+
+import java.util.List;
+
+public class GraphV2Test {
+    public static void main(String[] args) {
+        int v = 14;
+        GraphV2 graph = new GraphV2(v);
+        graph.addVertex(0,320,600);
+        graph.addVertex(1,300,630);
+        graph.addVertex(2,280,625);
+        graph.addVertex(3,270,630);
+        graph.addVertex(4,320,700);
+        graph.addVertex(5,360,620);
+        graph.addVertex(6,320,590);
+        graph.addVertex(7,370,580);
+        graph.addVertex(8,350,730);
+        graph.addVertex(9,390,690);
+        graph.addVertex(10,400,620);
+        graph.addVertex(11,420,580);
+        graph.addVertex(12,270,670);
+        graph.addVertex(13,270,600);
+
+        graph.addEdge(0,1,20);
+        graph.addEdge(0,4,60);
+        graph.addEdge(0,5,60);
+        graph.addEdge(0,6,60);
+
+        graph.addEdge(1,2,20);
+        graph.addEdge(2,3,10);
+        graph.addEdge(3,12,40);
+        graph.addEdge(3,13,30);
+
+        graph.addEdge(12,4,40);
+        graph.addEdge(4,8,50);
+        graph.addEdge(5,8,70);
+        graph.addEdge(5,9,80);
+        graph.addEdge(5,10,50);
+
+        graph.addEdge(8,9,50);
+        graph.addEdge(9,10,60);
+        graph.addEdge(13,6,50);
+        graph.addEdge(6,7,70);
+        graph.addEdge(7,11,50);
+
+        List<Integer>  path = graph.astar(0,10);
+        System.out.println(path);
+    }
+}
